aaa97d440cef20c4d8855f8cf24bb200ba20b4b6
finbar <farg967@aucklanduni.ac.nz>
2020-05-08 13:47:41 +1200

    Inlcuded paperPlotSetup for plot formatting

---
+++ paperPlotSetup.py
@@ -0,0 +1,55 @@
+import matplotlib as mpl
+import numpy as np
+
+def Setup_Plot(numCols):
+    mpl.rcParams['xtick.direction'] = 'in'
+    mpl.rcParams['ytick.direction'] = 'in'
+    # fontPath = os.path.join('C:\\', 'Users', 'fargus', 'Documents', 'pycharm_projects', 'xfoil', 'venv', 'Lib',
+    #                         'site-packages', 'matplotlib', 'mpl-data', 'fonts', 'ttf', 'cmunrm.ttf')
+
+    # if not os.path.exists(fontPath):
+    #     print('font path doesnt exist')
+    #     sys.exit()
+
+    # prop = font_manager.FontProperties(fname=fontPath)
+    # mpl.rcParams['font.family'] = prop.get_name()
+    mpl.rcParams['font.family'] = 'Times New Roman'
+    mpl.rcParams['mathtext.default'] = 'regular'
+    mpl.rcParams['axes.formatter.use_mathtext'] = True
+    mpl.rcParams['grid.color'] = 'xkcd:light grey'
+    mpl.rcParams['legend.edgecolor'] = 'k'
+    mpl.rcParams['legend.fancybox'] = False
+    # calculate figure width and height for number of columns
+    assert(numCols in [1,2])
+    if numCols == 1:
+        fig_width = 3.8
+        mpl.rcParams['font.size'] = 8
+        mpl.rcParams['xtick.labelsize'] = 8
+        mpl.rcParams['ytick.labelsize'] = 8
+        mpl.rcParams['axes.labelsize'] = 8
+        mpl.rcParams['legend.fontsize'] = 6
+        mpl.rcParams['lines.linewidth'] = 0.8
+    elif numCols ==2:
+        fig_width = 6.9
+        mpl.rcParams['font.size'] = 20
+        mpl.rcParams['xtick.labelsize'] = 16
+        mpl.rcParams['ytick.labelsize'] = 16
+        mpl.rcParams['axes.labelsize'] = 20
+        mpl.rcParams['legend.fontsize'] = 20
+        mpl.rcParams['lines.linewidth'] = 1.8
+    elif(numCols ==3):
+        fig_width = 10
+        mpl.rcParams['font.size'] = 20
+        mpl.rcParams['xtick.labelsize'] = 16
+        mpl.rcParams['ytick.labelsize'] = 16
+        mpl.rcParams['axes.labelsize'] = 20
+        mpl.rcParams['legend.fontsize'] = 20
+        mpl.rcParams['lines.linewidth'] = 1.8
+
+
+    # aesthetic ratio
+    golden_mean = (np.sqrt(5) - 1.0)/2.0
+    fig_height = fig_width * golden_mean
+    mpl.rcParams['figure.figsize'] = [fig_width, fig_height]
+
+
